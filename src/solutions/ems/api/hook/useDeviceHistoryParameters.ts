06aace397914c20879e4ba6dd1b53ad7486c07ed
治遠 林 <cyulin@sce.pccu.edu.tw>
2025-04-08 12:07:12 +0800

    analysis demand加上log

--- src/solutions/ems/api/hook/useDeviceHistoryParameters.ts
+++ src/solutions/ems/api/hook/useDeviceHistoryParameters.ts
@@ -5,12 +5,34 @@ export const useDeviceHistoryParameters = (filter: DeviceHistoryInput) => {
   const bothDevicIdLocationId =
     (filter.deviceId && filter.locationId) || (!filter.deviceId && !filter.locationId) ? true : false;
 
+  // 在 API 調用前記錄日誌，查看 filter 內容，特別是 paramName 是否為 'demand'
+  console.log('[useDeviceHistoryParameters] API Filter:', {
+    startDate: filter.startDate,
+    endDate: filter.endDate,
+    deviceId: filter.deviceId,
+    locationId: filter.locationId,
+    paramName: filter.paramName,
+    index: filter.index,
+    partLocationId: filter.partLocationId,
+    willCallAPI: !!filter.startDate && !!filter.endDate && bothDevicIdLocationId
+  });
+
   const { isLoading, isFetching, data } = useQuery(
     ['DeviceParametersHistory', filter],
-    () => apiGetDeviceParametersHistory(filter),
+    () => {
+      console.log('[useDeviceHistoryParameters] Calling API with paramName:', filter.paramName);
+      return apiGetDeviceParametersHistory(filter);
+    },
     {
       enabled: !!filter.startDate && !!filter.endDate && bothDevicIdLocationId,
       staleTime: 5 * (60 * 1000),
+      onSuccess: (data) => {
+        console.log('[useDeviceHistoryParameters] API Response Data:', { 
+          paramName: filter.paramName, 
+          dataLength: data?.length,
+          dataSample: data?.slice(0, 2) // 只記錄前兩條數據作為示例
+        });
+      },
     },
   );
 
