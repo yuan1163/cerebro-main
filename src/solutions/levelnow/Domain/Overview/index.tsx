551f49b04e7171445d4956f13cd0f7e2d0558e4e
zhduan <zhduan@g.sce.pccu.edu.tw>
2025-11-19 18:35:47 +0800

    修改 /levelnow/domain : dark mode

--- src/solutions/levelnow/Domain/Overview/index.tsx
+++ src/solutions/levelnow/Domain/Overview/index.tsx
@@ -41,64 +41,75 @@ export default observer(function Overview() {
           component='h3'
           variant='lg'
           weight='medium'
-          className='flex items-center tracking-32 text-[#000] h-[34px]'
+          className='flex items-center tracking-32 text-typography-primary h-[34px]'
         >
           {t('solutions.overview.label', 'Overview', "A summary of the domain's key features and functionalities.")}
         </Text>
       </CardHeader>
       <CardContent disablePaddingTop>
-        <div className='flex items-center justify-between bg-neutral-50 rounded-[10px] px-5 py-10'>
+        <div className='flex items-center justify-between bg-background dark:bg-surface-02 rounded-[10px] px-5 py-10'>
           {/* Left side stats */}
           <div className='flex flex-col gap-5'>
             {stats.map((stat) => (
               <div key={stat.label} className='flex items-center justify-between min-w-[135px]'>
-                <span className='text-sm font-medium tracking-wide text-secondary-500'>{stat.label}</span>
-                <span className='text-sm font-medium text-neutral-900'>{stat.value}</span>
+                <span className='text-sm font-medium tracking-wide text-secondary-500 dark:text-typography-secondary'>
+                  {stat.label}
+                </span>
+                <span className='text-sm font-medium text-neutral-900 dark:text-typography-secondary'>
+                  {stat.value}
+                </span>
               </div>
             ))}
           </div>
-          <div className='flex items-center gap-5'>
-            {/* Center donut chart */}
-            <div className='relative'>
-              <ResponsiveContainer width={160} height={160}>
-                <PieChart>
-                  <Pie
-                    data={data}
-                    cx='50%'
-                    cy='50%'
-                    innerRadius={56}
-                    outerRadius={80}
-                    paddingAngle={2}
-                    dataKey='value'
-                    startAngle={0}
-                    endAngle={-360}
-                  >
-                    {data.map((entry) => (
-                      <Cell key={entry.range} fill={entry.color} />
-                    ))}
-                  </Pie>
-                </PieChart>
-              </ResponsiveContainer>
-              <div className='absolute inset-0 flex flex-col items-center justify-center'>
-                <span className='text-base font-medium tracking-32 text-neutral-900'>
-                  {t('general.tanks.label', 'Tanks', 'Tanks')}
-                </span>
-                <span className='text-[40px] font-medium tracking-[0.8px] text-neutral-900'>{total}</span>
-              </div>
-            </div>
-
-            {/* Right side legend */}
-            <div className='flex flex-col gap-5'>
-              {data.map((item) => (
-                <div key={item.range} className='flex items-center justify-between gap-2'>
-                  <div className='w-2.5 aspect-square rounded-full' style={{ backgroundColor: item.color }} />
-                  <span className='text-sm font-medium text-secondary-500 min-w-[85px]'>{item.range}</span>
-                  <span className='text-sm font-medium text-neutral-900'>{item.value}</span>
-                </div>
-              ))}
+          {/* <div className='flex items-center gap-5'> */}
+          {/* Center donut chart */}
+          <div className='relative'>
+            <ResponsiveContainer width={160} height={160}>
+              <PieChart>
+                <Pie
+                  data={data}
+                  cx='50%'
+                  cy='50%'
+                  innerRadius={56}
+                  outerRadius={80}
+                  paddingAngle={2}
+                  dataKey='value'
+                  startAngle={0}
+                  endAngle={-360}
+                  stroke='none'
+                >
+                  {data.map((entry) => (
+                    <Cell key={entry.range} fill={entry.color} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+            <div className='absolute inset-0 flex flex-col items-center justify-center'>
+              <span className='text-base font-medium tracking-32 text-neutral-900 dark:text-typography-primary'>
+                {t('general.tanks.label', 'Tanks', 'Tanks')}
+              </span>
+              <span className='text-[40px] font-medium tracking-[0.8px] text-neutral-900 dark:text-typography-primary'>
+                {total}
+              </span>
             </div>
           </div>
+
+          {/* Right side legend */}
+          <div className='flex flex-col gap-5'>
+            {data.map((item) => (
+              <div key={item.range} className='flex items-center justify-between gap-2'>
+                <div className='w-2.5 aspect-square rounded-full' style={{ backgroundColor: item.color }} />
+                <span className='text-sm font-medium text-secondary-500 dark:text-typography-secondary min-w-[85px]'>
+                  {item.range}
+                </span>
+                <span className='text-sm font-medium text-neutral-900 dark:text-typography-secondary'>
+                  {item.value}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
+        {/* </div> */}
       </CardContent>
     </Card>
   );
